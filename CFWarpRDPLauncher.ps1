4764b904f2009e87a58c78f640e0b0312183bb0e
Jordan Johnson <lahma69@gmail.com>
2026-03-31 18:38:30 -0500

    Optimized CFWarpRDPLauncher.ps1

--- CFWarpRDPLauncher.ps1
+++ CFWarpRDPLauncher.ps1
@@ -12,29 +12,41 @@ param(
 
 function Kill-RdpProcesses {
     Write-Host "Killing all existing RDP processes..." -ForegroundColor Yellow
-    Stop-Process -Name "mstsc" -Force -ErrorAction SilentlyContinue
-    Stop-Process -Name "wksprt" -Force -ErrorAction SilentlyContinue
+    Stop-Process -Name "mstsc", "wksprt" -Force -ErrorAction SilentlyContinue
 }
 
 function Show-AuthInstructions {
-    Write-Host "Re-authentication is required to access '${softwareName}'. Please follow these steps: " -ForegroundColor Blue
-    Write-Host "- Look in the bottom right corner of your screen for a notification titled 'Cloudflare WARP' "
-    Write-Host "- Click the button labeled 'Open Browser and Re-Authenticate' "
-    Write-Host "- A new web browser tab/window should now open. If you do not see it, hover your mouse over "
-    Write-Host "  the web browser icon in your taskbar. Click the browser tab/window with the title including "
-    Write-Host "  the text 'Cloudflare Access' "
-    Write-Host "- If you see a success message on the webpage, continue to the next step. If you instead "
-    Write-Host "  see a login/sign-in webpage, do one of the following: "
-    Write-Host "    - If you have a work email address: "
-    Write-Host "        Click the 'Azure AD - Azure AD - Tex-Mix Concrete' button and enter your work email "
-    Write-Host "        credentials if prompted (if you see a success msg, you don't need to enter anything) "
-    Write-Host "    - If you do not have a work email address: "
-    Write-Host "        Enter your personal email address in the 'Email' textbox, click 'Send me a code', "
-    Write-Host "        and follow the instructions "
-    Write-Host "- Once you see a message indicating you have successfully authenticated, close the browser "
-    Write-Host "tab/window, click inside this window, and " -NoNewline
-    Write-Host "press any key on your keyboard to continue..." -ForegroundColor Blue -NoNewline
-    [System.Console]::ReadKey($true)
+    Write-Host "Re-authentication is required to access '${softwareName}'. Please follow these steps:" -ForegroundColor Red
+    Write-Host "- Look in the bottom right corner of your screen for a notification titled '" -ForegroundColor Blue -NoNewline
+    Write-Host "Cloudflare WARP" -ForegroundColor Cyan -NoNewline
+    Write-Host "'" -ForegroundColor Blue
+    Write-Host "- Click the button labeled '" -ForegroundColor Blue -NoNewline
+    Write-Host "Open Browser and Re-Authenticate" -ForegroundColor Cyan -NoNewline
+    Write-Host "'" -ForegroundColor Blue
+    Write-Host "- A new web browser tab/window should now open. If you do not see it, hover your mouse over" -ForegroundColor Blue
+    Write-Host "  the web browser icon in your taskbar. Click the browser tab/window with the title including" -ForegroundColor Blue
+    Write-Host "  the text '" -ForegroundColor Blue -NoNewline
+    Write-Host "Cloudflare Access" -ForegroundColor Cyan -NoNewline
+    Write-Host "'" -ForegroundColor Blue
+    Write-Host "- If you see a success message on the webpage, continue to the next step. If you instead" -ForegroundColor Blue
+    Write-Host "  see a login/sign-in webpage, do one of the following:" -ForegroundColor Blue
+    Write-Host "    - If you have a work email address:" -ForegroundColor Blue
+    Write-Host "        Click the '" -ForegroundColor Blue -NoNewline
+    Write-Host "Azure AD - Azure AD - Tex-Mix Concrete" -ForegroundColor Cyan -NoNewline
+    Write-Host "' button and enter your work email" -ForegroundColor Blue
+    Write-Host "        credentials if prompted (if you see a success msg, you don't need to enter anything)" -ForegroundColor Blue
+    Write-Host "    - If you do not have a work email address:" -ForegroundColor Blue
+    Write-Host "        Enter your personal email address in the '" -ForegroundColor Blue -NoNewline
+    Write-Host "Email" -ForegroundColor Cyan -NoNewline
+    Write-Host "' textbox, click '" -ForegroundColor Blue -NoNewline
+    Write-Host "Send me a code" -ForegroundColor Cyan -NoNewline
+    Write-Host "'," -ForegroundColor Blue
+    Write-Host "        and follow the instructions" -ForegroundColor Blue
+    Write-Host "- Once you see a message indicating you have successfully authenticated, close the browser" -ForegroundColor Blue
+    Write-Host "  tab/window, click inside this window, and " -ForegroundColor Blue -NoNewline
+    Write-Host "press any key on your keyboard to continue..." -ForegroundColor Yellow -NoNewLine
+    [System.Console]::ReadKey($true) | Out-Null
+    Write-Host ""  # New line after key press
 }
 
 function Test-Connect {
@@ -59,7 +71,6 @@ function Test-Connect {
     $rdpNegotiationByteArray = [byte[]] -split ('0300002c27e00000000000436f6f6b69653a206d737473686173683d656c746f6e730d0a0100080000000000' -replace '..', '0x$& ')
     $validResponseByteArray = [byte[]] -split ('030000130ED000001234' -replace '..', '0x$& ')
 
-    $serverIsRunningRdp = $false
     $tcpClient = New-Object System.Net.Sockets.TcpClient
 
     try {
@@ -70,68 +81,87 @@ function Test-Connect {
             $responseBuffer = [byte[]]::new(32)
             $responseLength = $networkStream.Read($responseBuffer, 0, $responseBuffer.Length)
             $networkStream.Close()
+            
             if ($responseLength -ge $validResponseByteArray.Length) {
-                $serverIsRunningRdp = Compare-ByteArrays $validResponseByteArray $responseBuffer $validResponseByteArray.Length
+                return Compare-ByteArrays $validResponseByteArray $responseBuffer $validResponseByteArray.Length
             }
         }
+        return $false
     }
     catch {
-        # Write-Host "An error occurred while attempting to connect: $_" -ForegroundColor Red
+        return $false
     }
     finally {
         $tcpClient.Close()
     }
-
-    return $serverIsRunningRdp
 }
 
 function Open-RdpFile {
-    param(
-        [string]$rdpPath
-    )
+    param([string]$rdpPath)
+    
     Write-Host "Launching '${softwareName}'. Please wait..." -ForegroundColor Green
     Start-Process "mstsc.exe" -ArgumentList "`"${rdpPath}`""
 }
 
-function Show-SuccessMsg {
-    Write-Host "'${softwareName}' should now be starting up." -ForegroundColor Green -NoNewLine
-    Write-Host " If it is not, try running this script again. "
-    Write-Host "If it still fails, please contact IT for help. This window will automatically close "
-    Write-Host "in ${autoCloseTimeout} seconds. To exit immediately, click inside this window, and "
-    Write-Host "press any key on your keyboard to exit..." -ForegroundColor Blue
-    Start-Sleep -Seconds $autoCloseTimeout
+function Show-ExitMessage {
+    param(
+        [bool]$success,
+        [bool]$waitForKey = $false
+    )
+    
+    if ($success) {
+        Write-Host "'${softwareName}' should now be starting up. If it is not, try running this script " -ForegroundColor Green
+        Write-Host "again. If it still fails, please contact IT for help. This window will automatically " -ForegroundColor Green
+        Write-Host "close in ${autoCloseTimeout} seconds. To exit immediately, click inside this window, and" -ForegroundColor Green
+        Write-Host "press " -ForegroundColor Yellow -NoNewLine
+        Write-Host "any key on your keyboard to exit..." -ForegroundColor Yellow
+        
+        # Wait for key or timeout, whichever comes first
+        $timeout = [datetime]::Now.AddSeconds($autoCloseTimeout)
+        while ([datetime]::Now -lt $timeout) {
+            if ([Console]::KeyAvailable) {
+                [System.Console]::ReadKey($true) | Out-Null
+                break
+            }
+            Start-Sleep -Milliseconds 100
+        }
+    }
+    else {
+        Write-Host "Communication with the '${softwareName}' server could not be established." -ForegroundColor Red
+        Write-Host "Try running this script again, and if it still fails, please contact"
+        Write-Host "IT for help. To close this window, click inside this window, and"
+        Write-Host "press any key on your keyboard to exit..." -ForegroundColor Yellow
+        
+        if ($waitForKey) {
+            [System.Console]::ReadKey($true) | Out-Null
+        }
+    }
 }
 
-function Show-FailMsg {
-    Write-Host "Communication with the '${softwareName}' server could not be established. " -ForegroundColor Red
-    Write-Host "Try running this script again, and if it still fails, please contact "
-    Write-Host "IT for help. To close this window, click inside this window, and "
-    Write-Host "press any key on your keyboard to exit..." -ForegroundColor Blue
-    [System.Console]::ReadKey($true)
-}
-
-function Wait-ToConnect {
+function Wait-ForConnection {
+    param([bool]$showInitialDelay = $false)
+    
     Write-Host "'${softwareName}' will open automatically as soon as a connection to the server is established. Please wait..." -ForegroundColor Yellow
-    Start-Sleep -Seconds 20
-}
-
-function Begin-Loop {
+    
+    if ($showInitialDelay) {
+        Start-Sleep -Seconds 20
+    }
+    
     $startTime = [datetime]::Now
-    $elapsedTime = 0
-    Write-Host "'${softwareName}' will open automatically as soon as a connection to the server is established. Please wait..." -ForegroundColor Yellow
-
+    
     while ($true) {
         $elapsedTime = ([datetime]::Now - $startTime).TotalSeconds
+        
         if ($elapsedTime -ge $rdpWaitTimeoutSec) {
             Write-Host "Timeout reached. Aborting..." -ForegroundColor Red
-            Show-FailMsg
+            Show-ExitMessage -success $false -waitForKey $true
             exit 1
         }
 
         if (Test-Connect -ipAddress $ip -port $port -connectTimeoutMs $connectTimeoutMs) {
             Write-Host "Connection successful!" -ForegroundColor Green
             Open-RdpFile -rdpPath $rdpPath
-            Show-SuccessMsg
+            Show-ExitMessage -success $true
             exit 0
         }
 
@@ -143,13 +173,16 @@ function Begin-Loop {
 if ($killAllRdpProcs) {
     Kill-RdpProcesses
 }
+
 Write-Host "Checking the connection to '${softwareName}'. Please wait..." -ForegroundColor Yellow
+
 if (Test-Connect -ipAddress $ip -port $port -connectTimeoutMs $connectTimeoutMs) {
     Write-Host "Connection successful!" -ForegroundColor Green
     Open-RdpFile -rdpPath $rdpPath
-    Show-SuccessMsg
+    Show-ExitMessage -success $true
     exit 0
-} else {
+}
+else {
     Show-AuthInstructions
-    Begin-Loop
+    Wait-ForConnection -showInitialDelay $true
 }
